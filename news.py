95ff97bab954fdeb274f312e8a3104dcfb2b13f0
melomen <30689352+limangox@users.noreply.github.com>
2023-08-05 07:07:19 +0900

    Update news.py

--- news.py
+++ news.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import streamlit as st
 
 news_url = st.text_input(label='请输入网址,图片在侧边栏 ')
-st.caption('*目前支持 日刊Sports | Oricon news | Mantan-Web*')
+st.caption('*目前支持 日刊Sports | Oricon news *')
 
 
 def nikkansports(news_url):
@@ -93,49 +93,7 @@ def oricon(url):
                 st.sidebar.image(og_img, width=350)
 
 
-def mantan(url):
-    if '/photopage' in url:
-        url = url.replace('/photopage', '.html')
-
-    resp = requests.get(url)
-    resp.encoding = 'utf-8'  # 指定UTF-8编码
-    html_content = resp.text
-
-    article_title = re.findall('<h1 class="article__title" .*?>(.*?)</h1>', html_content, re.S)[0]
-    st.subheader(article_title)
-
-    # 文章部分
-    soup = BeautifulSoup(html_content, 'html.parser')
-    all_p_tags = soup.find_all('p', class_='article__text')
-    result_text = '<br></br>'.join([p.get_text(strip=True) for p in all_p_tags])
-    st.markdown(result_text, unsafe_allow_html=True)
-    img_re = re.findall(
-        '<div class="swiper-slide" .*?><a aria-current="page"href=".*?"class="router-link-active router-link-exact-active photo__photolist-item" .*?><img src="(.*?)"alt="".*?></a></div>',
-        html_content, re.S)
-
-    # 图片部分
-
-    if '/photopage' not in url:
-        img_url = url.replace('.html','/photopage/001.html')
-
-        img_resp = requests.get(img_url).text
-        soup = BeautifulSoup(img_resp, 'html.parser')
-        swiper_slide_divs = soup.find_all('div', class_='swiper-slide')
-
-        # 遍历每个<div>标签，找到对应的<img>标签，并将图片链接的size6替换为size10
-        for div in swiper_slide_divs:
-            img_tag = div.find('img')
-            if img_tag:
-                src = img_tag['src']
-                new_src = src.replace('size6', 'size10')
-                img_tag['src'] = new_src
-                imgs = img_tag['src']
-                st.sidebar.image(imgs,width=300)
-
-
 if 'nikkansports' in news_url:
     nikkansports(news_url)
 if 'oricon' in news_url:
     oricon(news_url)
-if 'mantan' in news_url:
-    mantan(news_url)

2ad88945f7a8697b9d284006af98ef67faed5265
melomen <30689352+limangox@users.noreply.github.com>
2023-08-05 20:25:04 +0900

    Update news.py

--- news.py
+++ news.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import streamlit as st
 
 news_url = st.text_input(label='请输入网址,图片在侧边栏 ')
-st.caption('*目前支持 日刊Sports | Oricon news *')
+st.caption('*目前支持 日刊Sports | Oricon news | Mantan-Web*')
 
 
 def nikkansports(news_url):
@@ -93,7 +93,60 @@ def oricon(url):
                 st.sidebar.image(og_img, width=350)
 
 
+def mantan(url):
+    global url_new
+    global img_url
+
+    if '/photopage' in url:
+        url_new = url.replace('/photopage', '.html')
+        img_url = url.replace('.html', '/photopage/001.html')
+    if '/photopage' not in url:
+        url_new = url
+        img_url = url.replace('.html', '/photopage/001.html')
+
+    resp = requests.get(url_new)
+    resp.encoding = 'utf-8'  # 指定UTF-8编码
+    html_content = resp.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # 找到script标签中type属性为"application/ld+json"的内容
+    script_content = soup.find('script', {'type': 'application/ld+json'}).string
+
+    # 解析JSON内容
+    data = json.loads(script_content)
+
+    # 获取"headline"的值
+    article_title = data['headline']
+
+    st.subheader(article_title)
+
+    # 文章部分
+    all_p_tags = soup.find_all('p', class_='article__text')
+    result_text = '<br></br>'.join([p.get_text(strip=True) for p in all_p_tags])
+    st.markdown(result_text, unsafe_allow_html=True)
+    img_re = re.findall(
+        '<div class="swiper-slide" .*?><a aria-current="page"href=".*?"class="router-link-active router-link-exact-active photo__photolist-item" .*?><img src="(.*?)"alt="".*?></a></div>',
+        html_content, re.S)
+
+    # 图片部分
+
+    img_resp = requests.get(img_url).text
+    soup = BeautifulSoup(img_resp, 'html.parser')
+    img_script = soup.find('script', string=re.compile(r'var __images'))
+
+
+    pattern = r'"src": "(.*?)"'
+    src_list = re.findall(pattern, img_script.string)
+
+    for img in src_list:
+        st.sidebar.image(img,width=300)
+
+
+
 if 'nikkansports' in news_url:
     nikkansports(news_url)
 if 'oricon' in news_url:
     oricon(news_url)
+if 'mantan' in news_url:
+    mantan(news_url)
